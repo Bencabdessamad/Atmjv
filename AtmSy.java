80c562c7f2c73e9899412449864e38372546b1cf
abdessamad ben cheraik <121764744+Bencabdessamad@users.noreply.github.com>
2023-07-12 17:23:59 +0100

    Update AtmSy.java

--- AtmSy.java
+++ AtmSy.java
@@ -1,49 +1,56 @@
 import java.util.Scanner;
 
 //create atm class to implement the atm functionality
-public class AtmSy{
+public class AtmSystem {
     //main method
     public static void main(String[] args) {
-        //declare and intialize balance , withdraw and deposit
-        int balance = 100000, withdraw ,deposit;
-        try (
-            //create scanner class object to get choice of user
-        Scanner sc = new Scanner(System.in)) {
-            while(true){
-                System.out.println("Automated teller machine");
-                System.out.println("choose 1 for widthraw");
-                System.out.println("choose 2 for deposit");
-                System.out.println("choose 3 for check balance");
-                System.out.println("choose 4 for exit");
-                System.out.println("Choose the operating you want to perform :");
-            }
+        //declare and initialize balance, withdraw, and deposit
+        int balance = 100000, withdraw, deposit;
+        
+        //create scanner class object to get choice of user
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            System.out.println("Automated teller machine");
+            System.out.println("Choose 1 for withdrawal");
+            System.out.println("Choose 2 for deposit");
+            System.out.println("Choose 3 for check balance");
+            System.out.println("Choose 4 to exit");
+            System.out.println("Choose the operation you want to perform:");
+            
             //get choice from user
-             int choix;
-             choix=sc.nextInt();
-             
-            switch(choix){
+            int choice = sc.nextInt();
+            
+            switch (choice) {
                 case 1:
-                System.out.println("enter money to be widthrawn :");
-                withdraw = sc.nextInt();
-                if(balance >= withdraw){
-                    balance = balance - withdraw;
-                    System.out.println("Please collect your money");
-                }else{
-                    System.out.println("Insufficient balance");
-                }
-                System.out.println("");
-                break;
+                    System.out.println("Enter amount to be withdrawn:");
+                    withdraw = sc.nextInt();
+                    if (balance >= withdraw) {
+                        balance -= withdraw;
+                        System.out.println("Please collect your money.");
+                    } else {
+                        System.out.println("Insufficient balance.");
+                    }
+                    System.out.println();
+                    break;
                 case 2:
-                System.out.println("enter money to be deposited :");
-                deposit = sc.nextInt();
-                balance = balance + deposit;
-                System.out.println("your money has been successfully deposit");
-                System.out.println("");
-                break;
+                    System.out.println("Enter amount to be deposited:");
+                    deposit = sc.nextInt();
+                    balance += deposit;
+                    System.out.println("Your money has been successfully deposited.");
+                    System.out.println();
+                    break;
                 case 3:
-                System.out.println("your balance is : " + balance);
+                    System.out.println("Your balance is: " + balance);
+                    System.out.println();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+                    System.out.println();
+                    break;
             }
         }
-
     }
 }
